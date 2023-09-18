bd3d426defc07d7ce9198d7298b4a456b034423b
Jejomar Batitis <jejomar.batitis@ideascience.com>
2023-09-19 09:28:06 +1000

    DEVELOP: added sqlite jdbc package, files for db connection and creating user table

---
+++ src/database/CreateUserTable.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateUserTable {
+    public static void main(String[] args) {
+        final String TABLE_NAME = "Users";
+
+        try (Connection con = DatabaseConnection.getConnection();
+                Statement stmt = con.createStatement();) {
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " + TABLE_NAME
+                    + "(id INT NOT NULL,"
+                    + "first_name VARCHAR(20) NOT NULL,"
+                    + "last_name VARCHAR(20) NOT NULL,"
+                    + "role INT NOT NULL,"
+                    + "PRIMARY KEY (id))");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/database/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:sqlite:data_analytics.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+}
